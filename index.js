c8e79d3b1ddc20de699dd1d495469f99cc8b7c64
SureVX <ben@surevx.com>
2020-11-10 17:08:18 +0100

    changed greeting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 // req variable is the request, res is the response express sends back
 // here we're sending back some JSON
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
