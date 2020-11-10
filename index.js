72a2d7f66123ef688b7626358afeb0284d4c12e3
SureVX <ben@surevx.com>
2020-11-10 16:03:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// app is listening for get requests on '/'.
+// req variable is the request, res is the response express sends back
+// here we're sending back some JSON
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+//app.post      => send some info
+//app.put       => update ALL properties of somethign
+//app.delete    => delete something
+//app.patch     => update one or two properties (not all)
