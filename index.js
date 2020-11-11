805936af051e604532762d6b7f69655b988c07e5
SureVX <ben@surevx.com>
2020-11-11 11:41:07 +0100

    minor updates

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-// app is listening for get requests on '/'.
-// req variable is the request, res is the response express sends back
-// here we're sending back some JSON
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-//app.post      => send some info
-//app.put       => update ALL properties of somethign
-//app.delete    => delete something
-//app.patch     => update one or two properties (not all)
