5095105ce34faf720b5d99d3a442c2408dc32ab5
SureVX <ben@surevx.com>
2020-11-12 14:38:22 +0100

    minor updates

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const keys = require('../config/keys');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; 
+
+const User = mongoose.model('users');       // single argument means we're fetching from mongoose (two args is creating)
 
 passport.use(
     new GoogleStrategy(
@@ -10,9 +13,16 @@ passport.use(
             callbackURL: '/auth/google/callback',           // Google OAuth kicks the request back to this route
         }, 
         (accessToken, refreshToken, profile, done) => {
-            console.log('access token:', accessToken);
-            console.log('refresh token:', refreshToken);
-            console.log('profile:', profile);
+            User.findOne({ googleId: profile.id })
+                .then(existingUser => {
+                    if (existingUser) {
+                        done(null, existingUser);
+                    } else {
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+            });
         }
     )
 );
