9e0e0767f36196df3f85114db2494d11e9609b1f
SureVX <ben@surevx.com>
2020-11-16 09:20:46 +0100

    added payments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -10,24 +11,31 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-
-// CookieSession middleware creates a 30-day cookie
+app.use(bodyParser.json());
 app.use(
     cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,   // 30 days, 24hrs, 60 mins, 60 seconds, 1000 miliseconds. Nasty. 
+        maxAge: 30 * 24 * 60 * 60 * 1000,
         keys: [keys.cookieKey]
-    })
+    }),
 );
-
-// Passport middleware initialises and manages the authentication and 
-// checking of existing cookies against the users in db
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    // Express serves production assets
+    // like main.js or main.css
+    app.use(express.static('client/build'));
+
+    // Express serves index.html if it doesn't 
+    // recognise the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+app.listen(PORT);
