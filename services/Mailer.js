f983226cb3189e8c7b1fa5fe7f30da14a3767911
SureVX <ben@surevx.com>
2020-11-17 06:43:19 +0100

    minor updates

---
+++ services/Mailer.js
@@ -0,0 +1,54 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+
+class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
+
+        this.sgApi = sendgrid(keys.sendGridKey);    // Using sendgrid library directly to create and object with which to interact with SendGrid API.
+        this.from_email = new helper.Email('ben@surevx.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+        this.addContent(this.body);     // provided by SendGrid to register the body as the content of the email.
+        this.addClickTracking();        // we write this, but it's specified by SendGrid.
+        this.addRecipients();           // we write this too. This is also specified by SendGrid.
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        });
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);   // function defined by SendGrid. 
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+
+        const response = await this.sgApi.API(request);
+        return response;
+    }
+}
+
+module.exports = Mailer;
