587f4980846d0cb499a6db89c3d89f7e196631ef
SureVX <ben@surevx.com>
2020-11-18 08:55:31 +0100

    finished front end

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,19 +6,18 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
-const SurveyNew = () => <h2>SurveyNew</h2>
-
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
     componentDidMount() {
         this.props.fetchUser();
-    }
+    } 
 
     render() {
         return (
             <div className="container">
                 <BrowserRouter>
-                    <div>
+                    <div className="container">
                         <Header />
                         <Route exact path="/" component={Landing} />
                         <Route exact path="/surveys" component={Dashboard} />
---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,16 @@
+import { isInvalid } from "redux-form";
+
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default (emails) => {
+    const invalidEmails = emails
+        .split(',')
+        .map(email => email.trim())
+        .filter(email => re.test(email) === false);
+
+    if (invalidEmails.length) {
+        return `The following emails are invalid: ${invalidEmails}`;
+    }
+
+    return;
+};
